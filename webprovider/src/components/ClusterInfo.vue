c865d36c71802c2020a8d5f6ea9e55521615c149
Anton Semjonov <anton@semjonov.de>
2024-12-02 00:34:02 +0800

    wp: only show throughput when connected

--- webprovider/src/components/ClusterInfo.vue
+++ webprovider/src/components/ClusterInfo.vue
@@ -11,17 +11,20 @@ const { providers, throughput } = storeToRefs(cluster);
 
 <template>
 
-  <!-- show number of providers at broker -->
-  <p v-if="providers !== undefined">
-    Broker has {{ providers }} connected provider{{ providers > 1 ? "s" : "" }}.
-  </p>
+  <!-- show number of providers and current throughput at broker -->
+  <div v-if="providers !== undefined">
+    <p>
+      Broker has {{ providers }} connected provider{{ providers > 1 ? "s" : "" }}.
+    </p>
+    <p>
+      Current throughput: {{ throughput }} tasks/sec
+    </p>
+  </div>
   <p v-else>
     You're not connected to any Broker.
   </p>
 
   <!-- show current throughput -->
-   <p>
-    Current throughput: {{ throughput }} tasks/sec
-   </p>
+   
 
 </template>
