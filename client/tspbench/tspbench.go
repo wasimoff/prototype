f2445df9c37b45d0ca7a412258b6fc227832a9fd
Anton Semjonov <anton@semjonov.de>
2024-12-04 15:09:05 +0800

    client: add a go client app looping with tsp.wasm, replacement for benchmode

---
+++ client/tspbench/tspbench.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"sync/atomic"
+	"time"
+	"wasimoff/broker/net/pb"
+	"wasimoff/broker/net/transport"
+
+	"google.golang.org/protobuf/proto"
+)
+
+var broker string = "http://localhost:4080"
+var tspn int = 10
+var parallel int = 32
+
+func main() {
+
+	flag.IntVar(&tspn, "tsp", tspn, "tsp rand N")
+	flag.IntVar(&parallel, "p", parallel, "how many parallel tasks to have in-flight")
+	flag.StringVar(&broker, "wasimoff", broker, "URL to the Wasimoff Broker")
+	flag.Parse()
+
+	// open a websocket to the broker
+	messenger, err := transport.DialWasimoff(context.TODO(), broker)
+	if err != nil {
+		log.Fatalf("ERR: %s", err)
+	}
+	defer messenger.Close(nil)
+
+	// construct task structure once
+	task := &pb.WasiTaskArgs{
+		Binary: &pb.File{Ref: proto.String("tsp.wasm")},
+		Args:   []string{"tsp.wasm", "rand", fmt.Sprintf("%d", tspn)},
+	}
+
+	// use "tickets" to limit the number of concurrent tasks in-flight
+	tickets := make(chan struct{}, parallel)
+	for len(tickets) < cap(tickets) {
+		tickets <- struct{}{}
+	}
+
+	// count completed requests
+	counter := atomic.Uint64{}
+
+	for {
+		<-tickets
+		go func() {
+
+			result := &pb.WasiTaskResult{}
+			err := messenger.RequestSync(context.TODO(), task, result)
+			if err != nil {
+				time.Sleep(time.Second)
+			}
+
+			tickets <- struct{}{}
+			c := counter.Add(1)
+			fmt.Printf("\rrequests: %12d", c)
+
+		}()
+	}
+
+}
